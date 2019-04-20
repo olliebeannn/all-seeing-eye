9e37a61bd5368fdcb26785d9cf270e4340026907
Ollie Khakwani <okhakwani@gmail.com>
2019-04-20 09:01:32 -0400

    enabled scss, added Header component with basic styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import '../styles/reset.css';
+import Header from './Header/Header';
+
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<a href="/auth/google">Sign in with Google</a>
+				<Header />
 			</div>
 		);
 	}
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import styles from './Header.scss';
+
+class Header extends Component {
+	render() {
+		return (
+			<ul className="Header">
+				<li className="Header__item">
+					<a href="/watchlist">Watchlist</a>
+				</li>
+				<li className="Header__item">
+					<a href="/discover">Discover</a>
+				</li>
+				<li className="Header__item">
+					<a href="/seen">Seen</a>
+				</li>
+				<li className="Header__item">
+					<a href="/auth/google">Sign in with Google</a>
+				</li>
+			</ul>
+		);
+	}
+}
+
+export default Header;
