ec26909736e5655aeeec7842a25661e698c36468
Ollie Khakwani <okhakwani@gmail.com>
2019-04-27 09:31:22 -0400

    Added logic to extract main attributes and director name and set on MovieDetail state

--- client/src/components/MovieDetail/MovieDetail.js
+++ client/src/components/MovieDetail/MovieDetail.js
@@ -1,24 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 import '../../styles/base.scss';
 import './MovieDetail.scss';
 
 class MovieDetail extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			movieData: {}
+		};
+	}
+
 	componentDidMount() {
 		const movieId = this.props.match.params.id;
 		console.log(movieId);
 
 		axios.get(`/api/movie_detail/${movieId}`).then(res => {
-			console.log(res.data);
+			console.log('full data:', res.data);
 
-			const director = res.data.credits.crew.filter(
+			const selectedAttributes = [
+				'id',
+				'title',
+				'overview',
+				'vote_count',
+				'vote_average',
+				'release_date',
+				'credits',
+				'genres',
+				'runtime',
+				'original_language',
+				'credits'
+			];
+
+			let movieData = _.cloneDeep(_.pick(res.data, selectedAttributes));
+
+			// Extract director info from credits arrays
+			const directorArray = res.data.credits.crew.filter(
 				crewMem =>
 					crewMem.department === 'Directing' &&
 					crewMem.job === 'Director'
 			);
 
-			console.log(director);
+			if (directorArray.length > 0) {
+				console.log('called!');
+				movieData.directorName = directorArray[0].name;
+			}
+
+			console.log('movie data:', movieData);
+
+			this.setState({
+				movieData: movieData
+			});
 		});
 	}
 
