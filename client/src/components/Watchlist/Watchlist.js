23688271bd3bf20fa000ed6b523937544399d67a
Ollie Khakwani <ollie@donut.ai>
2019-04-28 11:02:10 -0400

    Started refactoring watchlist to connect to store so it can display saved movies

--- client/src/components/Watchlist/Watchlist.js
+++ client/src/components/Watchlist/Watchlist.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Watchlist extends Component {
+	// Process for displaying movies:
+	// After mount, get the updated user (use existing action creator?) to extract their watchlist
+	// Make series of calls to fetch basic movie details for watchlist movies (and put on local state?)
+	// Display those via movie components
+
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+
 	render() {
 		return <div>Watchlist</div>;
 	}
 }
 
-export default Watchlist;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(
+	mapStateToProps,
+	actions
+)(Watchlist);
