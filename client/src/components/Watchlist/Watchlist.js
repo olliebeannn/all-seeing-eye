9ef51e3db87aeb9f56028ce82ff58bbb3b358035
Ollie Khakwani <okhakwani@gmail.com>
2019-04-28 18:29:15 -0400

    Got Watchlist sending request to backend api when auth is loaded, extracting movieIds

--- client/src/components/Watchlist/Watchlist.js
+++ client/src/components/Watchlist/Watchlist.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
+
 import * as actions from '../../actions';
+import Movie from '../Movie/Movie';
 
 class Watchlist extends Component {
 	// Process for displaying movies:
 	// After mount, get the updated user (use existing action creator?) to extract their watchlist
-	// Make series of calls to fetch basic movie details for watchlist movies (and put on local state?)
+	// Call backend API to get movie details for watchlist movies (and put on local state?)
 	// Display those via movie components
 
 	componentDidMount() {
-		this.props.fetchUser();
+		// this.props.fetchUser();
+	}
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.auth !== this.props.auth) {
+			console.log('componentDidUpdate!');
+			console.log('props', this.props);
+
+			axios
+				.post('/api/watchlist/fetch', {
+					movieIds: this.props.auth.watchlist
+				})
+				.then(res => console.log(res));
+		}
 	}
 
 	render() {
