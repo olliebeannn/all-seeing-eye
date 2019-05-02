e47e6e72f7d8cf519b019b7420ed5052b971d710
Ollie Khakwani <okhakwani@gmail.com>
2019-05-02 09:37:59 -0400

    Updated Discover reducer to subscribe to removeFromWatchlist action so it updates UI when Remove from Watchlist is clicked

--- client/src/reducers/discoverReducer.js
+++ client/src/reducers/discoverReducer.js
@@ -1,10 +1,24 @@
-import { FETCH_DISCOVER } from '../actions/types';
+import _ from 'lodash';
+
+import { FETCH_DISCOVER, REMOVE_FROM_WATCHLIST } from '../actions/types';
 
 export default function(state = null, action) {
 	switch (action.type) {
 		case FETCH_DISCOVER:
-			console.log('inside fetch_discover in discoverReducer');
+			// console.log('inside fetch_discover in discoverReducer');
 			return action.payload || false;
+		case REMOVE_FROM_WATCHLIST:
+			// console.log('inside remove_from_watchlist inside discoverReducer');
+
+			let newState = _.cloneDeep(state);
+
+			newState.forEach(movie => {
+				if (movie.id === action.payload.movieId) {
+					movie.onWatchlist = false;
+				}
+			});
+
+			return newState;
 		default:
 			return state;
 	}
