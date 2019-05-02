1646bd65007363714319acc6388af30926dc2410
Ollie Khakwani <ollie@donut.ai>
2019-05-02 09:28:03 -0400

    Migrated Discover to use redux to store displayed movies state, still need to update to use reducer actions to add and remove movies

--- client/src/components/Discover/Discover.js
+++ client/src/components/Discover/Discover.js
@@ -4,51 +4,71 @@ import axios from 'axios';
 import _ from 'lodash';
 
 import styles from './Discover.scss';
+import * as actions from '../../actions';
 
 import Movie from '../Movie/Movie';
 
 class Discover extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			movies: []
-		};
-	}
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		movies: []
+	// 	};
+	// }
 
 	componentDidMount() {
-		axios.get('/api/discover/fetch').then(res => {
-			console.log(res);
+		// axios.get('/api/discover/fetch').then(res => {
+		// 	console.log(res);
+		//
+		// 	const selectedAttributes = [
+		// 		'id',
+		// 		'title',
+		// 		'overview',
+		// 		'vote_average',
+		// 		'genre_ids',
+		// 		'onWatchlist'
+		// 	];
+		//
+		// 	let newMoviesState = [...this.state.movies];
+		//
+		// 	res.data.forEach(movie => {
+		// 		newMoviesState.push(
+		// 			_.cloneDeep(_.pick(movie, selectedAttributes))
+		// 		);
+		// 	});
+		//
+		// 	this.setState({
+		// 		movies: newMoviesState
+		// 	});
+		// });
 
-			const selectedAttributes = [
-				'id',
-				'title',
-				'overview',
-				'vote_average',
-				'genre_ids',
-				'onWatchlist'
-			];
-
-			let newMoviesState = [...this.state.movies];
-
-			res.data.forEach(movie => {
-				newMoviesState.push(
-					_.cloneDeep(_.pick(movie, selectedAttributes))
-				);
-			});
-
-			this.setState({
-				movies: newMoviesState
-			});
-		});
+		this.props.loadDiscoverMovies();
 	}
 
 	render() {
 		let movieContent = 'Loading...';
 
-		if (this.state.movies.length > 0) {
+		// if (this.state.movies.length > 0) {
+		// 	movieContent = [];
+		//
+		// 	this.state.movies.forEach(movie =>
+		// 		movieContent.push(
+		// 			<Movie
+		// 				id={movie.id}
+		// 				title={movie.title}
+		// 				voteAverage={movie.vote_average}
+		// 				overview={movie.overview}
+		// 				key={movie.id}
+		// 				onWatchlist={movie.onWatchlist}
+		// 			/>
+		// 		)
+		// 	);
+		// }
+
+		if (this.props.discoverList && this.props.discoverList.length > 0) {
 			movieContent = [];
 
-			this.state.movies.forEach(movie =>
+			this.props.discoverList.forEach(movie =>
 				movieContent.push(
 					<Movie
 						id={movie.id}
@@ -74,4 +94,7 @@ function mapStateToProps({ discoverList }) {
 	return { discoverList };
 }
 
-export default connect(mapStateToProps)(Discover);
+export default connect(
+	mapStateToProps,
+	actions
+)(Discover);
