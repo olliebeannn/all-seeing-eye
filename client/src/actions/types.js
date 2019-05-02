1e75594dbc178082d171ea471fe4fb5bdcb5920c
Ollie Khakwani <ollie@donut.ai>
2019-05-02 09:47:58 -0400

    Got Discover to update UI when user clicks on Add to Watchlist

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,7 @@
 export const FETCH_USER = 'fetch_user';
+
 export const FETCH_WATCHLIST = 'fetch_watchlist';
+export const ADD_TO_WATCHLIST = 'add_to_watchlist';
 export const REMOVE_FROM_WATCHLIST = 'remove_from_watchlist';
+
 export const FETCH_DISCOVER = 'fetch_discover';
--- client/src/reducers/discoverReducer.js
+++ client/src/reducers/discoverReducer.js
@@ -1,24 +1,43 @@
 import _ from 'lodash';
 
-import { FETCH_DISCOVER, REMOVE_FROM_WATCHLIST } from '../actions/types';
+import {
+	FETCH_DISCOVER,
+	ADD_TO_WATCHLIST,
+	REMOVE_FROM_WATCHLIST
+} from '../actions/types';
 
 export default function(state = null, action) {
 	switch (action.type) {
 		case FETCH_DISCOVER:
 			// console.log('inside fetch_discover in discoverReducer');
 			return action.payload || false;
+		case ADD_TO_WATCHLIST:
+			console.log(
+				'inside add_to_watchlist in discoverReducer',
+				action.payload.movieId
+			);
+
+			let addedState = _.cloneDeep(state);
+
+			addedState.forEach(movie => {
+				if (movie.id === action.payload.movieId) {
+					movie.onWatchlist = true;
+				}
+			});
+
+			return addedState;
 		case REMOVE_FROM_WATCHLIST:
 			// console.log('inside remove_from_watchlist inside discoverReducer');
 
-			let newState = _.cloneDeep(state);
+			let removedState = _.cloneDeep(state);
 
-			newState.forEach(movie => {
+			removedState.forEach(movie => {
 				if (movie.id === action.payload.movieId) {
 					movie.onWatchlist = false;
 				}
 			});
 
-			return newState;
+			return removedState;
 		default:
 			return state;
 	}
