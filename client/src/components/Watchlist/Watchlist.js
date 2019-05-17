0eab788676a738f43217ac7a6ab232a7f5d61a34
Ollie Khakwani <okhakwani@gmail.com>
2019-05-17 15:47:13 -0400

    Fixed watchlist to stop showing movie after removeFromWatchlist is clicked

--- client/src/components/Watchlist/Watchlist.js
+++ client/src/components/Watchlist/Watchlist.js
@@ -10,23 +10,29 @@ import MovieList from '../MovieList/MovieList';
 import styles from './Watchlist.scss';
 
 class Watchlist extends Component {
-	render() {
-		return (
-			<div className="Watchlist">
-				<MovieList movies={this.props.watchlist} />
-			</div>
-		);
-	}
+  render() {
+    let moviesToDisplay = null;
+
+    if (this.props.watchlist) {
+      moviesToDisplay = this.props.watchlist.filter(movie => movie.onWatchlist);
+    }
+
+    return (
+      <div className="Watchlist">
+        <MovieList movies={moviesToDisplay} />
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		auth: state.auth,
-		watchlist: state.watchlist
-	};
+  return {
+    auth: state.auth,
+    watchlist: state.watchlist
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	actions
+  mapStateToProps,
+  actions
 )(Watchlist);
--- client/src/reducers/watchlistReducer.js
+++ client/src/reducers/watchlistReducer.js
@@ -3,32 +3,32 @@ import _ from 'lodash';
 import { FETCH_WATCHLIST, REMOVE_FROM_WATCHLIST } from '../actions/types';
 
 export default function(state = null, action) {
-	// console.log('watchlist reducer called!');
+  // console.log('watchlist reducer called!');
 
-	switch (action.type) {
-		case FETCH_WATCHLIST:
-			// console.log('inside fetchWatchlist action in reducer');
-			return action.payload || false;
-		case REMOVE_FROM_WATCHLIST:
-			// console.log('inside removeFromWatchlist action in reducer', action);
-			// console.log(
-			// 	'inside removeFromWatchlist action in reducer, movieId',
-			// 	action.payload.movieId
-			// );
-			//
-			// console.log('state', state);
+  switch (action.type) {
+    case FETCH_WATCHLIST:
+      // console.log('inside fetchWatchlist action in reducer');
+      return action.payload || false;
+    case REMOVE_FROM_WATCHLIST:
+      // console.log('inside removeFromWatchlist action in reducer', action);
+      // console.log(
+      // 	'inside removeFromWatchlist action in reducer, movieId',
+      // 	action.payload.movieId
+      // );
+      //
+      // console.log('state', state);
 
-			let newState = _.cloneDeep(state);
+      let newState = _.cloneDeep(state);
 
-			newState.forEach(movie => {
-				if (movie.id === action.payload.movieId) {
-					movie.onWatchlist = false;
-				}
-			});
+      newState.forEach(movie => {
+        if (movie.movieId === action.payload.movieId) {
+          movie.onWatchlist = false;
+        }
+      });
 
-			return newState;
-		default:
-			// console.log('called watchlistReducer with default!', state);
-			return state;
-	}
+      return newState;
+    default:
+      // console.log('called watchlistReducer with default!', state);
+      return state;
+  }
 }
