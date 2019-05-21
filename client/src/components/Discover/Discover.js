629accfed0973e590b3ebea412da8769d93168a2
Ollie Khakwani <okhakwani@gmail.com>
2019-05-21 08:59:41 -0400

    Added range component from module, have it updating Discover state onAfterChange

--- client/src/components/Discover/Discover.js
+++ client/src/components/Discover/Discover.js
@@ -4,17 +4,26 @@ import axios from 'axios';
 import _ from 'lodash';
 
 import styles from './Discover.scss';
+// Slider component styles
+import 'rc-slider/assets/index.css';
+
 import * as actions from '../../actions';
 
 import Movie from '../Movie/Movie';
 import MovieList from '../MovieList/MovieList';
 import Button from '../Button/Button';
 
+const Slider = require('rc-slider');
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
 class Discover extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPage: 1
+      currentPage: 1,
+      releaseDateMin: 1900,
+      releaseDateMax: 2019
     };
   }
 
@@ -30,6 +39,10 @@ class Discover extends Component {
     this.setState({ currentPage: page + 1 });
   };
 
+  handleReleaseYearChange = sliderValues => {
+    console.log(sliderValues);
+  };
+
   render() {
     let moviesToDisplay = null;
 
@@ -39,6 +52,17 @@ class Discover extends Component {
 
     return (
       <div className="Discover">
+        <div className="Discover__filters">
+          <Range
+            min={1900}
+            max={2019}
+            defaultValue={[
+              this.state.releaseDateMin,
+              this.state.releaseDateMax
+            ]}
+            onAfterChange={this.handleReleaseYearChange}
+          />
+        </div>
         <MovieList movies={moviesToDisplay} />
         <div className="Discover__loadMore">
           <Button
