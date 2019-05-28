74032790241142dfcd404b643209728aaa2c3d56
Ollie Khakwani <okhakwani@gmail.com>
2019-05-27 22:11:15 -0400

    Small style changes to App and Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import styles from './Header.scss';
+import './Header.scss';
 
 class Header extends Component {
   renderContent() {
-    let headerContent = <li className="Header__item" key="7" />;
+    let headerContent = <li className="Header__item" key="8" />;
 
     switch (this.props.auth) {
       case null:
         break;
       case false:
         headerContent = (
-          <li className="Header__item" key="6">
+          <li className="Header__item" key="7">
             <a href="/auth/google">Sign in with Google</a>
           </li>
         );
         break;
       default:
         headerContent = [
-          <li className="Header__item Header__tabLink" key="2">
+          <li className="Header__item Header__tabLink" key="3">
             <a href="/watchlist">Watchlist</a>
           </li>,
-          <li className="Header__item Header__tabLink" key="3">
+          <li className="Header__item Header__tabLink" key="4">
             <a href="/discover">Discover</a>
           </li>,
-          <li className="Header__item Header__tabLink" key="4">
+          <li className="Header__item Header__tabLink" key="5">
             <a href="/seen">Seen</a>
           </li>,
-          <li className="Header__item" key="5">
+          <li className="Header__item" key="6">
             <a href="/api/logout">Sign Out</a>
           </li>
         ];
@@ -43,7 +43,12 @@ class Header extends Component {
         <li className="Header__item Header__menuIcon" key="0">
           <i className="material-icons">menu</i>
         </li>
-        <li className="Header__item Header__appName" key="1">
+        <li className="Header__item Header__logo" key="1">
+          <img
+            src={process.env.PUBLIC_URL + '/images/logo-all-seeing-eye.svg'}
+          />
+        </li>
+        <li className="Header__item Header__appName" key="2">
           <a href="/watchlist">The All Seeing Eye</a>
         </li>
         {this.renderContent()}
