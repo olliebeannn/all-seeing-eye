13acffc22c247f68d95b1d8a60cdf89587e4d9a6
Ollie Khakwani <okhakwani@gmail.com>
2019-05-28 09:21:38 -0400

    Added ability to redirect to Discover from EmptyList

--- client/src/components/EmptyList/EmptyList.js
+++ client/src/components/EmptyList/EmptyList.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './EmptyList.scss';
 
 import Button from '../Button/Button';
 
-const EmptyList = props => {
-  return (
-    <div className="EmptyList flex flex--ai--c flex--dir-c">
-      <img className="EmptyList__image" src={props.image} />
-      <h2 className="EmptyList__text white center mt2">{props.text}</h2>
-      <Button className="Button Button--primary mt2">{props.buttonText}</Button>
-    </div>
-  );
-};
+class EmptyList extends Component {
+  render() {
+    return (
+      <div className="EmptyList flex flex--ai--c flex--dir-c">
+        <img className="EmptyList__image" src={this.props.image} />
+        <h2 className="EmptyList__text white center mt2">{this.props.text}</h2>
+        <Button
+          click={() => {
+            this.props.history.push('/discover');
+          }}
+          className="Button Button--primary mt2"
+        >
+          {this.props.buttonText}
+        </Button>
+      </div>
+    );
+  }
+}
 
-export default EmptyList;
+export default withRouter(EmptyList);
