3b1099172cb6341df1f9bfb0a7cb67dae88320f1
Ollie Khakwani <okhakwani@gmail.com>
2019-06-14 18:12:22 -0400

    Fixed toasts to be responsive

--- client/src/components/Toasts/Toasts.js
+++ client/src/components/Toasts/Toasts.js
@@ -8,10 +8,15 @@ const Toasts = props => {
   let toastsContent = null;
 
   if (props.toasts.length > 0) {
+    // toastsContent = (
+    //   <ul>
+    //     {props.toasts.map(toast => <Toast text={toast.text} key={toast.id} />)}
+    //   </ul>
+    // );
     toastsContent = (
-      <ul>
+      <>
         {props.toasts.map(toast => <Toast text={toast.text} key={toast.id} />)}
-      </ul>
+      </>
     );
   }
 
