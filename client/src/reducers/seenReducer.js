9df225d28fa693bf5a7b8418d6b637a93da7c21a
Ollie Khakwani <okhakwani@gmail.com>
2019-06-22 12:28:52 -0400

    got Seen updating Watchlist state in real time

--- client/src/reducers/seenReducer.js
+++ client/src/reducers/seenReducer.js
@@ -1,6 +1,11 @@
 import _ from 'lodash';
 
-import { FETCH_SEEN, REMOVE_FROM_SEEN } from '../actions/types';
+import {
+  FETCH_SEEN,
+  REMOVE_FROM_SEEN,
+  ADD_TO_WATCHLIST,
+  REMOVE_FROM_WATCHLIST
+} from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
@@ -16,6 +21,26 @@ export default function(state = [], action) {
       });
 
       return newState;
+    case ADD_TO_WATCHLIST:
+      let watchlistAddedState = _.cloneDeep(state);
+
+      watchlistAddedState.forEach(movie => {
+        if (movie.movieId === action.payload.movieId) {
+          movie.onWatchlist = true;
+        }
+      });
+
+      return watchlistAddedState;
+    case REMOVE_FROM_WATCHLIST:
+      let watchlistRemovedState = _.cloneDeep(state);
+
+      watchlistRemovedState.forEach(movie => {
+        if (movie.movieId === action.payload.movieId) {
+          movie.onWatchlist = false;
+        }
+      });
+
+      return watchlistRemovedState;
     default:
       return state;
   }
