afaca20bc4c9bfd877f65db843db37fb48208164
Ollie Khakwani <okhakwani@gmail.com>
2019-06-28 14:54:03 -0400

    refactored Landing to redirect to Watchlist if user logged in

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import './Landing.scss';
 
-const Landing = props => {
-  return (
-    <div className="Landing flex flex--dir-c flex--jc--c flex--ai--c center white">
-      <p className="Landing__Header">
-        Never agonize over what movie to watch again.
-      </p>
-      <h2 className="Landing__Subhead mt2">
-        Find movies and save them for later so they’re waiting for you when you
-        have time.
-      </h2>
-      <button href="/auth/google" className="Landing__Button mt4">
-        Sign Up
-      </button>
-    </div>
-  );
-};
+class Landing extends Component {
+  render() {
+    if (this.props.auth) {
+      return <Redirect to="/watchlist" />;
+    }
 
-export default Landing;
+    return (
+      <div className="Landing flex flex--dir-c flex--jc--c flex--ai--c center white">
+        <p className="Landing__Header">
+          Never agonize over what movie to watch again.
+        </p>
+        <h2 className="Landing__Subhead mt2">
+          Find movies and save them for later so they’re waiting for you when
+          you have time.
+        </h2>
+        <button href="/auth/google" className="Landing__Button mt4">
+          Sign Up
+        </button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Landing);
